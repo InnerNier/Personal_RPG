b1ccd1a40f1eb095d6179d465e673ef534f51e8a
xande <xande@XanderCPU>
2026-05-24 20:56:44 -0500

    1st True Commit

--- My_Video_Game/src/main_files/Main.java
+++ My_Video_Game/src/main_files/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-		JFrame frame = new JFrame("My Video Game"); //readying
+		JFrame frame = new JFrame("My Video Game"); //readying commit
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setSize(400, 300);
 	    frame.setLocationRelativeTo(null);
